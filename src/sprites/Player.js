26d1c6f04fd1cab102fdf6c52e7da57640e46d29
Val Isautier <val.isautier@gmail.com>
2018-07-23 15:59:20 +0200

    Players can now be stunned when colliding with Rudi

--- src/sprites/Player.js
+++ src/sprites/Player.js
@@ -2,7 +2,7 @@ import Phaser from 'phaser'
 import { animDuration } from '../utils'
 
 export default class Player extends Phaser.Sprite {
-  constructor ({ game, x, y, atlas, input, color, pos, /* hasCarnetAsset,*/ disabledAsset, stunAsset, stunCarnetAsset }) {
+  constructor ({ game, x, y, atlas, input, color, pos, disabledAsset }) {
     super(game, x, y, atlas)
     this.xOffset = 0.7
 
@@ -13,7 +13,7 @@ export default class Player extends Phaser.Sprite {
 
     this.defaultAngle = 200
     this.bodySize = 50
-    this.didInput = false
+    this.updateSpeed = false
 
     this.hasCarnet = false
     this.collisionTimer = null
@@ -22,13 +22,12 @@ export default class Player extends Phaser.Sprite {
     this.score = 0
     this.speed = 200
 
-    this.stunAsset = stunAsset
-    this.stunCarnetAsset = stunCarnetAsset
     this.stunTimer = null
     this.stunTimerDuration = 3000
 
     this.animations.add('defaultMove', Phaser.Animation.generateFrameNames('perso-0-', 0, 5))
     this.animations.add('carnetMove', Phaser.Animation.generateFrameNames('persoCarnet-', 0, 5))
+    this.animations.add('stunMove', Phaser.Animation.generateFrameNames('persoStun-', 0, 3))
     this.animations.play('defaultMove', 1 / (animDuration / 1000), true)
     this.scale.setTo(0.5)
 
@@ -69,19 +68,26 @@ export default class Player extends Phaser.Sprite {
         this.loadTexture(this.stunCarnetAsset, 0, false)
       } else { */
       // }
-    } else if (this.frameName.indexOf('perso-0-') === -1 && !this.hasCarnet) {
-      // If the player loses the Carnet and is playing another animation, change back to default animation.
+    } 
+
+    if (this.frameName.indexOf('perso-0-') === -1 && !this.hasCarnet) {
+      // If the player loses the Carnet and is playing another animation, change to default animation.
       this.animations.play('defaultMove', 1 / (animDuration / 1000), true)
     }
     
-    /* else if (this.collisionTimer.running) {
-      this.loadTexture(this.disabledAsset, 0, false)
-    } else if (this.key === this.disabledAsset) {
+     /*else if (this.key === this.disabledAsset) {
       // this.loadTexture(this.defaultAsset, 0, false)
     }*/ 
   }
 
   checkStun () {
+    if (this.frameName.indexOf('persoStun-') === -1 && this.stunTimer.running) {
+      // If the stun timer is running and another animation is playing, change to Stun animation.
+      this.animations.play('stunMove', 1 / (animDuration / 1000), true)
+    } else if (this.frameName.indexOf('persoStun-') !== -1 && !(this.stunTimer.running)) {
+      this.animations.play('defaultMove', 1 / (animDuration / 1000), true)
+    }
+
     /* if (this.stunTimer.running) {
       this.loadTexture(this.stunAsset, 0, false)
     } else if (this.key === this.stunAsset) {
@@ -93,17 +99,20 @@ export default class Player extends Phaser.Sprite {
     if (this.stunTimer.running) {
       this.speed = 50;
     } else {
+      if (this.speed === 50) {
+        this.updateSpeed = true
+      }
       this.speed = 200;
     }
 
     if (this.game.input.keyboard.isDown(Phaser.Keyboard[this.playerInput])) {
       this.body.angularVelocity = -this.defaultAngle;
-      this.didInput = true;
+      this.updateSpeed = true;
     } else {
-      if (this.didInput) {
+      if (this.updateSpeed) {
         this.body.angularVelocity = 0;
         this.game.physics.arcade.velocityFromAngle(this.angle, this.speed, this.body.velocity);
-        this.didInput = false;
+        this.updateSpeed = false;
       }
 
       if (this.body.blocked.up || this.body.blocked.down) {
@@ -117,7 +126,7 @@ export default class Player extends Phaser.Sprite {
 
   stun () {
     this.stunTimer.start();
-    this.didInput = true;
+    this.updateSpeed = true;
   }
 
   updateScore () {
